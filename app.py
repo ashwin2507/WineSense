a8c88ab89ccd49884ad07c45b41398baf3b31a2f
Ashwin Babu <ash2507@student.ubc.ca>
2023-03-21 18:50:08 -0700

    Edited final design

--- app.py
+++ app.py
@@ -6,24 +6,6 @@ import plotly.graph_objs as go
 import plotly.express as px
 
 
-# wine_df = pd.read_csv("data/clean/cleaned_wine_data.csv")
-
-# world_df = wine_df.groupby('country').first().reset_index() 
-# fig = px.scatter_geo(world_df, locations="iso_alpha", color="continent",
-#                      hover_name="country", size = 'Wine_count', projection="natural earth")
-# fig.show()
-
-
-# app = dash.Dash()
-# app.layout = html.Div([
-#     dcc.Graph(figure=fig)
-# ])
-
-# app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
-
-
-
-
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
